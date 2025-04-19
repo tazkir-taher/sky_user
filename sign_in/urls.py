b091da58a298801e6a6203692a17646403179dbf
Tazkir <tazkir912@gmail.com>
2025-04-19 22:29:04 +0600

    added dashboard and session summery

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_user, name='register'),
+    path('dashboard/<int:user_id>/', views.user_dashboard, name='dashboard'), 
 ]
--- sign_in/views.py
+++ sign_in/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import UserForm
+from django.shortcuts import get_object_or_404
+from .models import User
+
+def user_dashboard(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    success_message = None
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            success_message = "Profile updated successfully!"
+    else:
+        form = UserForm(instance=user)
+
+    return render(request, 'user_dashboard.html', {'form': form, 'success_message': success_message})
+
 
 def register_user(request):
     success_message = None
